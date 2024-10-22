dba9e0e4f1e2aa84571ea47e2ae37ae80ead8abb
Pranjal Maheshwari <91134337+pranjalmaheshwarii@users.noreply.github.com>
2024-10-22 11:40:20 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "google" {
+  credentials = file("${path.module}/black-outlet-438804-p8.json")
+  project     = "black-outlet-438804-p8"
+  region      = "us-central1"
+  zone        = "us-central1-a"
+}
+
+resource "google_compute_instance" "software_automation_vm" {
+  name         = "software-automation-vm"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP for external access
+    }
+  }
+
+  metadata = {
+    ssh-keys = "your-ssh-user:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  tags = ["jenkins", "cicd"]
+}
